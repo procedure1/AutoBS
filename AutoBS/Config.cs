85fbffe92345c69746a0edb397849650d520066c
procedure1 <bradwallace51@gmail.com>
2026-03-02 20:30:46 -0800

    arc swing mode default changed

--- AutoBS/Config.cs
+++ AutoBS/Config.cs
@@ -74,7 +74,7 @@ namespace AutoBS
         public virtual ArcRotationModeType ArcRotationMode { get; set; } = ArcRotationModeType.NetZero;
 
         public enum ArcSwingModeType { Curated180and135, Curated180and135and90, All180and135, All180and135and90 }
-        public virtual ArcSwingModeType ArcSwingMode { get; set; } = ArcSwingModeType.Curated180and135;
+        public virtual ArcSwingModeType ArcSwingMode { get; set; } = ArcSwingModeType.Curated180and135and90;
         //public virtual bool ForceNaturalArcs { get; set; } = true; //False will allow more variety in arc connections between the head and tail notes. Some of these arcs will take a less natural path since they are 135 degree changes instead of 180 degree changes.
         public virtual float PreferredArcCountPerMin { get; set; } = 12.0f;//how many arcs per minute to aim for.
         public virtual float MinArcDuration { get; set; } = 0.9f;
